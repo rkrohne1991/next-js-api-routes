0bc6f8b873f96e82b95c56b814fba61e73e643d0
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-12-10 07:19:29 +0000

    Changes to json data and fixing spelling mistake

--- pages/api/[feedbackId].js
+++ pages/api/[feedbackId].js
@@ -1,7 +1,7 @@
 import { buildFeedbackPath, extractFeedback } from "./feedback";
 
 function handler(req, res) {
-  const feedbackId = req.query.feedbakcId;
+  const feedbackId = req.query.feedbackId;
   const filePath = buildFeedbackPath();
   const feedbackData = extractFeedback(filePath);
   const selectedFeedback = feedbackData.find(
